50f6bd25a3e13b8bded6176a41b5bc1b9ff085bc
Luna <eduardaluna22@gmail.com>
2019-11-07 03:04:36 -0300

    apagando arquivos com referencia a usuario

--- test/controllers/usuarios_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UsuariosControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get usuarios_new_url
-    assert_response :success
-  end
-
-end
