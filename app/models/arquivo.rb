6b62cfd6f77ba0d9faecbe43f7b4c9187c9e1985
Luna <eduardaluna22@gmail.com>
2019-12-16 22:14:30 -0300

    consertando bugs

--- app/models/arquivo.rb
+++ app/models/arquivo.rb
@@ -9,12 +9,12 @@ class Arquivo < ApplicationRecord
   end
 
   def self.download_arquivo(diretorio)
-    Zip::File.open(diretorio+'.zip', Zip::File::CREATE) do
+    Zip::File.open(diretorio+'.zip', Zip::File::CREATE) do |arquivo_zip|
       Dir.chdir diretorio
-      #Dir.glob("**/*").reject { |e| File.directory?(e) }.each do |arquivo|
-      #  puts "Adicionando #{arquivo}"
-      #  arquivo_zip.add(arquivo.sub(diretorio + '/', ''), arquivo)
-      #end
+      Dir.glob("**/*").reject { |e| File.directory?(e) }.each do |arquivo|
+        puts "Adicionando #{arquivo}"
+        arquivo_zip.add(arquivo.sub(diretorio + '/', ''), arquivo)
+      end
     end
   end
 end
